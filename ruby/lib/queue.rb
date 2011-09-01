93502eaa9d8658268686cc6b998f2e14fce15274
bcoe <bencoe@gmail.com>
2011-09-01 12:46:08 -0700

    Building queue class in Ruby.

--- ruby/lib/queue.rb
+++ ruby/lib/queue.rb
@@ -1,5 +1,65 @@
+require 'mongo'
+
 module Karait
   class Queue
+    
+    include Karait
+    
+    def initialize(opts={})
+      set_instance_variables opts
+      create_mongo_connection
+    end
+    
+    def write(message, routing_key=nil, expire=-1.0)
+      if message.type == Hash
+        message_dict = message
+      end
+      
+      message_dict[:_meta] = {
+        :expire => expire,
+        :timestamp => Time.now().to_f
+      }
+      
+      @queue_collection.insert(message_dict, :safe => true)
+    end
+    
+    private
+    
+    def set_instance_variables(opts)
+      
+      defaults = {
+        :host => 'localhost',
+        :port => 27017,
+        :database => 'karait',
+        :queue => 'messages',
+        :average_message_size => 8192,
+        :queue_size => 4096
+      }.merge(opts)
+            
+      defaults.each do |k,v|
+        add_accessible_attribute(k, v)
+      end
+      
+    end
+    
+    def create_mongo_connection
+      @connection = Mongo::Connection.new(
+        self.host,
+        self.port
+      )
+      @database = @connection[self.database]
+      create_capped_collection
+      @queue_collection = @database[self.queue]
+    end
+    
+    def create_capped_collection
+      @database.create_collection(
+        self.queue,
+        :size => (self.average_message_size * self.queue_size),
+        :capped => true,
+        :max => self.queue_size
+      )
+    end
     
   end
 end
--- ruby/test/test_queue.rb
+++ ruby/test/test_queue.rb
@@ -1,9 +1,84 @@
 require 'helper'
+require 'mongo'
 require 'karait'
 
 class TestQueue < Test::Unit::TestCase
-  should "initialize a capped collection when a queue is created" do
-    queue = Karait::Queue.new
-    assert true
+  
+  def setup
+    Mongo::Connection.new()['karait_test']['queue_test'].drop()
   end
+  
+  should "initialize queue with parameters" do
+    queue = Karait::Queue.new :average_message_size => 20
+    assert_equal 20, queue.average_message_size
+    
+    queue = Karait::Queue.new
+    assert_equal 8192, queue.average_message_size
+  end
+  
+  should "initialize a capped collection when a queue is created" do
+    queue = Karait::Queue.new(
+      :database => 'karait_test',
+      :queue => 'queue_test',
+      :average_message_size => 8192,
+      :queue_size => 4096
+    )
+    
+    options = Mongo::Connection.new()['karait_test']['queue_test'].options
+    
+    assert_equal true, options['capped']
+    assert_equal 4096, options['max']
+    assert_equal (8192 * 4096), options['size']
+  end
+  
+  should "attach to a MongoDB collection that already exists" do
+    collection = Mongo::Connection.new()['karait_test']['queue_test']
+    collection.insert({
+            :message => {
+                :apple => 3,
+                :banana => 5
+            },
+            :_meta => {
+              :timestamp => 2523939,
+              :expire => 20393,
+              :routing_key => 'foo_key'
+            }
+    })
+    assert_equal 1, collection.count()
+    
+    queue = Karait::Queue.new(
+      :database => 'karait_test',
+      :queue => 'queue_test'
+    )
+
+    collection = Mongo::Connection.new()['karait_test']['queue_test']
+    assert_equal 1, collection.count()
+    assert_equal nil, collection.options()
+  end
+  
+  should "should write a dictionary into the MongoDB queue" do
+    queue = Karait::Queue.new(
+      :database => 'karait_test',
+      :queue => 'queue_test',
+      :average_message_size => 8192,
+      :queue_size => 4096
+    )
+    
+    queue.write({
+        :apple => 5,
+        :banana => 6,
+        :inner_object => {
+            :foo => 1,
+            :bar => 2
+        }
+    })
+
+    collection = Mongo::Connection.new()['karait_test']['queue_test']
+    obj = collection.find_one()
+    assert_equal 6, obj['banana']
+    assert_equal 2, obj['inner_object']['bar']
+    assert obj['_meta']['expire']
+    assert obj['_meta']['timestamp']
+  end
+  
 end

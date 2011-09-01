e6e9631610fce00655008339cc9e2864640f6f90
bcoe <bencoe@gmail.com>
2011-09-01 13:37:14 -0700

    Starting to build out Message class for Ruby client.

--- ruby/lib/message.rb
+++ ruby/lib/message.rb
@@ -1,5 +1,32 @@
 module Karait
   class Message
     
+    include Karait
+    
+    VARIABLE_REGEX = /^([a-z_][a-zA-Z_0-9]*)=$/
+    
+    def initialize(raw_message={})
+      @variables_to_serialize = {}
+      add_accessors raw_message
+    end
+        
+    private
+    
+    def add_accessors(hash)
+      hash.each do |k, v|
+        add_accessible_attribute k, v
+        @variables_to_serialize[k] = true
+      end
+    end
+    
+    def method_missing(id, *args)
+      if matches = id.to_s.match(Message::VARIABLE_REGEX) and args.count == 1
+        add_accessible_attribute(matches[1], args[0])
+        @variables_to_serialize[matches[1]] = true
+      else
+        raise NoMethodError
+      end
+    end
+    
   end
 end
--- ruby/test/test_message.rb
+++ ruby/test/test_message.rb
@@ -1,9 +1,27 @@
 require 'helper'
 require 'karait'
+require 'yaml'
 
-class TestQueue < Test::Unit::TestCase
-  should "return instance variables as a hash when to_hash is called" do
-    queue = Karait::Message.new
-    assert true
+class TestMessage < Test::Unit::TestCase
+  should "should add attribute accessors when initialized with a raw message" do
+    message = Karait::Message.new(
+      raw_message={
+        :apple => 5,
+        :banana => 3
+      }
+    )
+    
+    message.apple = 6
+    assert_equal 6, message.apple
+    assert_equal 3, message.banana
+  end
+  
+  should "add new attribute accessors for missing methods" do
+    message = Karait::Message.new(raw_message={
+        :apple => 5,
+        :banana => 3
+    })
+    message.foo = 9
+    assert_equal 9, message.foo
   end
 end

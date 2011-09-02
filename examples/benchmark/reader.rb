acceabf8a4c3202d942a8c5d2c5e3284d4f703cd
bcoe <bencoe@gmail.com>
2011-09-02 11:13:18 -0700

    Simplified benchmark example.

--- examples/benchmark/reader.rb
+++ examples/benchmark/reader.rb
@@ -12,14 +12,13 @@ while true
   messages = queue.read :routing_key => 'for_ruby_reader_writer', :messages_read => 15
   messages.each do |message|
     messages_read += 1.0
+        
+    message.messages_read = messages_read
+    message.messages_read_per_second = messages_read / (Time.now().to_f - start_time)
     
     if (messages_read % 250) == 0.0
       puts "Message Read: \n#{message.to_hash.to_yaml}"
     end
-    
-    message.messages_read = messages_read
-    message.messages_read_per_second = messages_read / (Time.now().to_f - start_time)
-    queue.write message
   end
   queue.delete_messages messages
 end
